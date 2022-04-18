e427c87513e56233f25fef056e77c565ee250982
Damien Laureaux <d.laureaux@timoa.com>
2022-04-18 11:39:56 +0200

    fix(terraform): add empty project files

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+#############################
+# Global
+#############################
+
+variable "namespace" {
+  type        = string
+  description = "Project name that will be use to identifiy the resources"
+  default     = "my-project"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage/environment name to tag and suffix the resources"
+  default     = "dev"
+}
