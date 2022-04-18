9085375a6946a6047cc59eed3028f61f47dde059
Damien Laureaux <d.laureaux@timoa.com>
2022-04-18 12:06:47 +0200

    feat(proxmox): configure the Proxmox provider

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,37 @@ variable "stage" {
   description = "Stage/environment name to tag and suffix the resources"
   default     = "dev"
 }
+
+#############################
+# Proxmox
+#############################
+
+variable "pm_host" {
+  type        = string
+  description = "Proxmox hostname or IP address"
+  default     = "localhost"
+}
+
+variable "pm_port" {
+  type        = string
+  description = "Proxmox port"
+  default     = 8006
+}
+
+variable "pm_user" {
+  type        = string
+  description = "Proxmo username"
+  default     = "root"
+}
+
+variable "pm_password" {
+  type        = string
+  description = "Password define during Proxmox installation"
+  default     = "my-secure-password"
+}
+
+variable "pm_tls_insecure" {
+  type        = bool
+  description = "Check for TLS certificate"
+  default     = false
+}
