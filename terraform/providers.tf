58fafcad9eca21c07a92149c3137c1add9816fda
Damien Laureaux <d.laureaux@timoa.com>
2022-04-27 19:12:42 +0200

    feat(microk8s): create the HomeLab VM with MicroK8S (WIP)

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,4 @@
 provider "proxmox" {
-  pm_api_url      = "https://${var.proxmox_host}:${var.proxmod_port}/api2/json"
-  pm_user         = var.proxmox_user
-  pm_password     = var.proxmox_password
-  pm_tls_insecure = var.proxmox_tls_insecure
+  pm_api_url      = "https://${var.pm_host}:${var.pm_port}/api2/json"
+  pm_tls_insecure = var.pm_tls_insecure
 }
